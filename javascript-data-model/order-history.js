b17f1a2593487a18f1e1ad5acf6746204d3634e7
Justin Song <justinsong1608@gmail.com>
2023-01-31 17:54:16 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,38 @@
+var orderHistory = [
+  {
+    productName: 'JavaScript for impatient programmers',
+    priceBeforeFees: '$31.55',
+    totalPrice: '$34.00',
+    orderNum: '114-3941689-8772232',
+    orderDate: 'August 4, 2020',
+    deliveredDate: 'August 8, 2020',
+    returnWindowDeadline: 'September 7, 2020'
+  },
+  {
+    productName: 'The Timeless Way of Building',
+    priceBeforeFees: '$41.33',
+    totalPrice: '$44.53',
+    orderNum: '113-9984268-1280257',
+    orderDate: 'July 19, 2020',
+    deliveredDate: 'July 20, 2020',
+    returnWindowDeadline: 'August 19, 2020'
+  },
+  {
+    productName: 'Gamecube Controller Adapter...',
+    priceBeforeFees: '$15.98',
+    totalPrice: '$17.22',
+    orderNum: '114-2875557-9059409',
+    orderDate: 'July 4, 2020',
+    deliveredDate: ' July 7, 2020',
+    returnWindowDeadline: 'August 5, 2020'
+  },
+  {
+    productName: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
+    priceBeforeFees: ['$94.95', '$33.99'],
+    totalPrice: '$138.93',
+    orderNum: '113-2883177-2648248',
+    orderDate: 'July 3, 2020',
+    deliveredDate: 'July 5, 2020',
+    returnWindowDeadline: 'August 4, 2020'
+  }
+];
