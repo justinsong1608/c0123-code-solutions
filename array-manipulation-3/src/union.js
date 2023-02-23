89315a7210c22371d988242b45036b1340f4335e
Justin Song <justinsong1608@gmail.com>
2023-02-23 00:42:06 +0000

    Completed array-manipulation-3 exercise.

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,11 @@
 /* exported union */
+function union(first, second) {
+  var togetherArr = first.concat(second);
+  var newArr = [];
+  for (var i = 0; i < togetherArr.length; i++) {
+    if (!newArr.includes(togetherArr[i])) {
+      newArr.push(togetherArr[i]);
+    }
+  }
+  return newArr;
+}
