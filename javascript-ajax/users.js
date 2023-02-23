2c52523a66eb316674802a9e13baf0ea6a41fd1e
Justin Song <justinsong1608@gmail.com>
2023-02-23 21:49:54 +0000

    Completed javascript-ajax exercise.

---
+++ javascript-ajax/users.js
@@ -0,0 +1,14 @@
+var $users = document.querySelector('#user-list');
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (var i = 0; i < xhr.response.length; i++) {
+    var $list = document.createElement('li');
+    $list.textContent = xhr.response[i].name;
+    $users.appendChild($list);
+  }
+});
+xhr.send();
