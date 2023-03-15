917ae087604dbc6066c95c46d1da15d55b9e4c82
Justin Song <justinsong1608@gmail.com>
2023-03-15 16:19:10 +0000

    Updated code based on suggestions.

--- node-module-system/calculate.js
+++ node-module-system/calculate.js
@@ -3,22 +3,26 @@ import subtract from './subtract.js';
 import multiply from './multiply.js';
 import divide from './divide.js';
 
-const [, , firstNum, , secondNum] = process.argv;
+const [, , firstNum, operator, secondNum] = process.argv;
 const num1 = Number(firstNum);
 const num2 = Number(secondNum);
-switch (process.argv[3]) {
+let result;
+switch (operator) {
   case 'plus':
-    console.log('result:', add(num1, num2));
+    result = add(num1, num2);
     break;
   case 'minus':
-    console.log('result:', subtract(num1, num2));
+    result = subtract(num1, num2);
     break;
   case 'over':
-    console.log('result:', divide(num1, num2));
+    result = divide(num1, num2);
     break;
   case 'times':
-    console.log('result:', multiply(num1, num2));
+    result = multiply(num1, num2);
     break;
   default:
     console.log('invalid operation');
+}
+if (result !== undefined) {
+  console.log('result:', result);
 }
