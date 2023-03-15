ba6e0368ac69b717dede7ebd9d1a086ba135ff62
Justin Song <justinsong1608@gmail.com>
2023-03-15 21:08:09 +0000

    Suggested edits.

--- es6-promises/main.js
+++ es6-promises/main.js
@@ -1,10 +1,9 @@
 import takeAChance from './take-a-chance.js';
 
-const user = takeAChance('Justin');
-
-user.then((value) => {
-  console.log(value);
-})
+takeAChance('Justin')
+  .then((value) => {
+    console.log(value);
+  })
   .catch((reason) => {
     console.error(reason.message);
   });
