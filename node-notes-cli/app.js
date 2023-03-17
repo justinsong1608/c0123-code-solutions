78e7f1033b7d3d8cb62daac1c98636ca4f4e1c2d
Justin Song <justinsong1608@gmail.com>
2023-03-17 17:12:41 +0000

    Completed node-notes-cli exercise.

---
+++ node-notes-cli/app.js
@@ -0,0 +1,75 @@
+import * as fs from 'node:fs/promises';
+
+async function readNotes() {
+  try {
+    const fileContent = await fs.readFile('data.json', 'utf-8');
+    const data = JSON.parse(fileContent);
+    const notes = Object.entries(data.notes)
+      .map(([id, note]) => `${id}: ${note}`)
+      .join('\n');
+    console.log(`${notes}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+async function createNote() {
+  try {
+    const newNote = process.argv[3];
+    const fileContent = await fs.readFile('data.json', 'utf8');
+    const data = JSON.parse(fileContent);
+    const newNoteId = data.nextId;
+    data.notes[newNoteId] = newNote;
+    data.nextId++;
+    await fs.writeFile('data.json', JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+async function updateNote() {
+  try {
+    const updateId = process.argv[3];
+    const updateNote = process.argv[4];
+    const fileContent = await fs.readFile('data.json', 'utf8');
+    const data = JSON.parse(fileContent);
+    data.notes[updateId] = updateNote;
+    await fs.writeFile('data.json', JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+async function deleteNote() {
+  try {
+    const deleteId = process.argv[3];
+    const fileContent = await fs.readFile('data.json', 'utf8');
+    const data = JSON.parse(fileContent);
+    delete data.notes[deleteId];
+    await fs.writeFile('data.json', JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+const action = process.argv[2];
+switch (action) {
+  case 'read':
+    readNotes();
+    break;
+  case 'create':
+    createNote();
+    break;
+  case 'update':
+    updateNote();
+    break;
+  case 'delete':
+    deleteNote();
+    break;
+  default:
+    throw console.error('Command not found!');
+}
