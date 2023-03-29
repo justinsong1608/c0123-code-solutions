14fa70503d8ae7ac95f1055e447b7de8d7a82461
Justin Song <justinsong1608@gmail.com>
2023-03-29 23:22:26 +0000

    Completed react-rendering-lists exercise.

---
+++ react-rendering-lists/justin-app/src/PokemonList.js
@@ -0,0 +1,7 @@
+export default function PokemonList( {pokedex} ) {
+  const listItems = pokedex.map(pokemon =>
+    <li key={pokemon.number}>{pokemon.name}</li>
+  );
+
+  return <ul>{listItems}</ul>
+}
