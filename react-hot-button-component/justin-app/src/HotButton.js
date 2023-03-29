45ab934409991e7cd007f735453d42ee2e7fd299
Justin Song <justinsong1608@gmail.com>
2023-03-29 18:28:28 +0000

    Updated based on suggestions.

--- react-hot-button-component/justin-app/src/HotButton.js
+++ react-hot-button-component/justin-app/src/HotButton.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 import './HotButton.css';
 
 export default function HotButton() {
@@ -8,20 +8,16 @@ export default function HotButton() {
     setClicked(clicked + 1);
   }
 
+  let color = "";
+
   switch (true) {
-    case (clicked >= 3 && clicked < 6):
-      return <button className="Hot-button-dark" onClick={handleClick}> EXPLODE </button>;
-    case (clicked >= 6 && clicked < 9):
-      return <button className="Hot-button-purple" onClick={handleClick}> EXPLODE </button>;
-    case (clicked >= 9 && clicked < 12):
-      return <button className="Hot-button-red" onClick={handleClick}> EXPLODE </button>;
-    case (clicked >= 12 && clicked < 15):
-      return <button className="Hot-button-orange" onClick={handleClick}> EXPLODE </button>;
-    case (clicked >= 15 && clicked < 18):
-      return <button className="Hot-button-yellow" onClick={handleClick}> EXPLODE </button>;
-    case (clicked >= 18):
-      return <button className="Hot-button-white" onClick={handleClick}> EXPLODE </button>;
-    default:
-      return <button className="Hot-button-black" onClick={handleClick}> EXPLODE </button>;
+    case (clicked >= 3 && clicked < 6): color = "Hot-button-dark"; break;
+    case (clicked >= 6 && clicked < 9): color = "Hot-button-purple"; break;
+    case (clicked >= 9 && clicked < 12): color = "Hot-button-red"; break;
+    case (clicked >= 12 && clicked < 15): color = "Hot-button-orange"; break;
+    case (clicked >= 15 && clicked < 18): color = "Hot-button-yellow"; break;
+    case (clicked >= 18): color = "Hot-button-white"; break;
+    default: color = "Hot-button-black"; break;
   }
+  return <button className={ color } onClick={handleClick}> EXPLODE </button>;
 }
