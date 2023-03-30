30b536315a876d549c0869e371cfd9d07afda321
Justin Song <justinsong1608@gmail.com>
2023-03-30 17:55:02 +0000

    Completed react-validated-input-component exercise.

---
+++ react-validated-input-component/justin-app/src/ValidatedInput.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+export default function ValidatedInput() {
+  const [input, setInput] = useState('');
+
+  const digit = /\d+/;
+  const digitRes = digit.test(input);
+
+  const capital = /[A-Z]+/;
+  const capitalRes = capital.test(input);
+
+  const escape = /[!,@ ,# ,$ ,& ,^ ,& ,* ,( ,)]+/;
+  const escapeRes = escape.test(input);
+
+  const goodLength = input.length > 7;
+
+  let text = 'A password is required.';
+  let check = "❌";
+
+  if (input.length > 0 && input.length < 8) {
+    text = 'Your password is too short.';
+  } else if (!digitRes && goodLength) {
+    text = 'Your password needs at least one number.';
+  } else if (!capitalRes && goodLength) {
+    text = 'Your password needs at least on capital letter.';
+  } else if (!escapeRes && goodLength) {
+    text = 'Your password needs at least on special character: !, @, #, $, %, ^, &, *, (, or ).;'
+  } else if (goodLength) {
+    text = '';
+    check = "✅";
+  }
+
+  return (
+    <form>
+      <label style={{display: "block" }}>Password</label>
+      <input value={input} onChange={e => setInput(e.target.value)}></input>
+      {check}
+      <div style={{color: "red"}}> {text} </div>
+    </form>
+  );
+}
