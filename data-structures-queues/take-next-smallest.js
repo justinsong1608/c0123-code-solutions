6744dbbfff394f67200c69eab57632004b3d514b
Justin Song <justinsong1608@gmail.com>
2023-04-13 18:27:50 +0000

    Robert's solution.

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -10,4 +10,12 @@ export default function takeNextSmallest(numberQueue) {
     }
   }
   return answer;
+
+  // Robert's solution
+  // let nextSmallest = numberQueue.dequeue();
+  // while (nextSmallest > numberQueue.peek()) {
+  //   numberQueue.enqueue(nextSmallest);
+  //   nextSmallest = numberQueue.dequeue();
+  // }
+  // return nextSmallest;
 }
